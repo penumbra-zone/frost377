c439a41f6a0c4b6b3a9b766ffa5a456ec3208434
Ava Howell <ava@penumbra.zone>
2022-02-27 17:58:19 -0800

    remove a few unnecessary clones, refactor polynomial evaluation

--- src/keygen.rs
+++ src/keygen.rs
@@ -7,25 +7,26 @@ use std::collections::BTreeMap;
 // - more documentation
 // - rework API design, think about api misuse potential
 // - add secure delete/zeroize-on-drop
-use ark_ff::{Field, PrimeField, UniformRand, Zero};
+use ark_ff::{PrimeField, UniformRand, Zero};
 use rand::thread_rng;
 
 #[derive(Debug, Clone)]
 pub struct RoundOne {
     /// commitments to <ai0, ai1, ai2 ...
     commitments: Vec<decaf377::Element>,
-    index: u32,
+    from_participant: u32,
 
     // proof of knowledge to ai0
     ri: decaf377::Element,
     ui: decaf377::Fr,
 }
 
+
 #[derive(Debug, Clone)]
-pub struct DKGKey {
-    verification_share: decaf377::Element,
-    private_share: decaf377::Fr,
-    group_public_key: decaf377::Element,
+pub struct RoundTwo {
+    pub for_participant: u32,
+    pub from_participant: u32,
+    pub share: Share,
 }
 
 #[derive(Debug, Clone)]
@@ -34,6 +35,7 @@ pub struct Share {
     pub y: decaf377::Fr,
 }
 
+#[derive(Debug, Clone)]
 pub struct DKGOutput {
     pub group_public_key: decaf377::Element,
     pub private_share: decaf377::Fr,
@@ -42,54 +44,28 @@ pub struct DKGOutput {
 
 #[derive(Clone)]
 pub struct Participant {
-    ri: decaf377::Element,
-    ui: decaf377::Fr,
     secret_coeffs: Vec<decaf377::Fr>,
     pub commitments: Vec<decaf377::Element>,
     pub index: u32,
 
     my_share: Share,
-    other_shares: Vec<Share>,
+    counterparty_shares: Vec<Share>,
     counterparty_commitments: BTreeMap<u32, Vec<decaf377::Element>>,
 }
 
 // evaluates the polynomial specified by `coeffs` using Horner's method
 // (https://en.wikipedia.org/wiki/Horner%27s_method) at x
-fn evaluate_polynomial(x: decaf377::Fr, coeffs: Vec<decaf377::Fr>) -> decaf377::Fr {
-    let mut result = decaf377::Fr::zero();
-
-    for (i, coeff) in coeffs.iter().rev().enumerate() {
-        result += coeff;
-
-        if i != coeffs.len() - 1 {
-            result *= x;
-        }
-    }
-    result
+fn evaluate_polynomial(x: decaf377::Fr, coeffs: &[decaf377::Fr]) -> decaf377::Fr {
+    coeffs
+        .iter()
+        .rev()
+        .fold(decaf377::Fr::zero(), |acc, coeff| acc * x + coeff)
 }
 
 impl Participant {
     pub fn new(index: u32, t: u32) -> Self {
         let rng = &mut thread_rng();
-
         let secret_coeffs = (0..t).map(|_| decaf377::Fr::rand(rng)).collect::<Vec<_>>();
-
-        let aio = secret_coeffs[0].clone();
-        let aio_commitment = aio * decaf377::basepoint();
-
-        // compute a proof of knowledge for ai0
-        let k = decaf377::Fr::rand(&mut thread_rng());
-        let ri = k * decaf377::basepoint();
-        let ci = blake2b_simd::Params::default()
-            .personal(b"keygen_challenge")
-            .to_state()
-            .update(&index.to_le_bytes())
-            .update("TODO: ANTI-REPLAY CONTEXT".as_bytes())
-            .update(aio_commitment.compress().0.as_ref())
-            .update(ri.compress().0.as_ref())
-            .finalize();
-        let ui = k + aio * decaf377::Fr::from_le_bytes_mod_order(ci.as_bytes());
-
         let public_commitments = secret_coeffs
             .iter()
             .map(|coeff| coeff * decaf377::basepoint())
@@ -99,35 +75,48 @@ impl Participant {
             commitments: public_commitments,
             my_share: Share {
                 x: index,
-                y: evaluate_polynomial(decaf377::Fr::from(index), secret_coeffs.clone()),
+                y: evaluate_polynomial(decaf377::Fr::from(index), &secret_coeffs),
             },
             secret_coeffs,
             index,
-            ri: ri,
-            ui: ui,
-            other_shares: Vec::new(),
+            counterparty_shares: Vec::new(),
             counterparty_commitments: BTreeMap::new(),
         }
     }
     pub fn round_one(&self) -> RoundOne {
+        let aio_commitment = self.secret_coeffs[0] * decaf377::basepoint();
+
+        // compute a proof of knowledge for ai0
+        let k = decaf377::Fr::rand(&mut thread_rng());
+        let ri = k * decaf377::basepoint();
+        let ci = blake2b_simd::Params::default()
+            .personal(b"keygen_challenge")
+            .to_state()
+            .update(&self.index.to_le_bytes())
+            .update("TODO: ANTI-REPLAY CONTEXT".as_bytes())
+            .update(aio_commitment.compress().0.as_ref())
+            .update(ri.compress().0.as_ref())
+            .finalize();
+
+        let ui = k + self.secret_coeffs[0] * decaf377::Fr::from_le_bytes_mod_order(ci.as_bytes());
         RoundOne {
             commitments: self.commitments.clone(),
-            ri: self.ri,
-            ui: self.ui,
-            index: self.index,
+            ri,
+            ui,
+            from_participant: self.index,
         }
     }
     pub fn verify_roundone(&mut self, others: Vec<RoundOne>) -> Result<(), anyhow::Error> {
         let context_string = "TODO: ANTI-REPLAY CONTEXT";
-        for (i, participant) in others.iter().enumerate() {
-            if participant.index == self.index {
+        for participant in others.iter() {
+            if participant.from_participant == self.index {
                 continue;
             }
             // verify RoundOne.ri = g^ui * theta_0^-cl
             let ci = blake2b_simd::Params::default()
                 .personal(b"keygen_challenge")
                 .to_state()
-                .update(&participant.index.to_le_bytes())
+                .update(&participant.from_participant.to_le_bytes())
                 .update(context_string.as_bytes())
                 .update(participant.commitments[0].compress().0.as_ref())
                 .update(participant.ri.compress().0.as_ref())
@@ -143,16 +132,13 @@ impl Participant {
 
             // store this participant's commitments
             self.counterparty_commitments
-                .insert(participant.index, participant.commitments.clone());
+                .insert(participant.from_participant, participant.commitments.clone());
         }
         Ok(())
     }
 
     pub fn round_two(&self, counterparty_index: u32) -> RoundTwo {
-        let fi = evaluate_polynomial(
-            decaf377::Fr::from(counterparty_index),
-            self.secret_coeffs.clone(),
-        );
+        let fi = evaluate_polynomial(decaf377::Fr::from(counterparty_index), &self.secret_coeffs);
 
         RoundTwo {
             for_participant: counterparty_index,
@@ -174,25 +160,24 @@ impl Participant {
             .ok_or(anyhow::anyhow!("counterparty commitments not found"))?;
 
         // verify that the round two response is correct
-        counterparty_response.verify(commitments.clone())?;
-        println!("verified round two response");
+        counterparty_response.verify(commitments)?;
 
-        self.other_shares.push(counterparty_response.share.clone());
+        self.counterparty_shares
+            .push(counterparty_response.share.clone());
 
         Ok(())
     }
 
     pub fn finalize(&self) -> Result<DKGOutput, anyhow::Error> {
-        println!("shares: {:?}", self.other_shares);
         // compute the group's public key
         let mut group_public_key = self.commitments[0];
-        for (i, commitment) in self.counterparty_commitments.iter() {
+        for commitment in self.counterparty_commitments.values() {
             group_public_key = group_public_key + commitment[0];
         }
 
         // compute the private share
         let mut private_share = self.my_share.y;
-        for other_share in self.other_shares.iter() {
+        for other_share in self.counterparty_shares.iter() {
             private_share = private_share + other_share.y;
         }
 
@@ -204,31 +189,22 @@ impl Participant {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct RoundTwo {
-    pub for_participant: u32,
-    pub from_participant: u32,
-    pub share: Share,
-}
-
 impl RoundTwo {
     pub fn verify(
         &self,
-        counterparty_commitments: Vec<decaf377::Element>,
+        counterparty_commitments: &[decaf377::Element],
     ) -> Result<(), anyhow::Error> {
         // step 2: verify the counterparty's shares, abort if the check fails
         // compute fl(i)*G
         let gfli = self.share.y * decaf377::basepoint();
 
         // verify fl(i)*G = sum(Cl(k) * i^k)
-        let mut result = decaf377::Element::default();
-        for (i, commitment) in counterparty_commitments.iter().rev().enumerate() {
-            result += commitment;
-
-            if i != counterparty_commitments.len() - 1 {
-                result = result * decaf377::Fr::from(self.for_participant);
-            }
-        }
+        let result = counterparty_commitments
+            .iter()
+            .rev()
+            .fold(decaf377::Element::default(), |acc, commitment| {
+                acc * decaf377::Fr::from(self.for_participant) + commitment
+            });
         if result != gfli {
             Err(anyhow::anyhow!("verification failed"))?
         }
@@ -290,9 +266,7 @@ mod tests {
                     continue;
                 }
                 let round2_message = participant_other.round_two(participant.index);
-
                 println!("verifying round two message: {:?}", round2_message);
-
                 participant
                     .verify_and_add_roundtwo_response(&round2_message)
                     .unwrap();
